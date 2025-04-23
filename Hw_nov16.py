6f078ee113383d4dc78f7fce0bd49921946b3043
RMX_NUL <vanya130211@gmail.com>
2025-04-23 18:16:49 +0300

    add a 2 classel

---
+++ Hw_nov16.py
@@ -0,0 +1,10 @@
+from pydantic import BaseModel, Field, conint, constr
+from typing import List
+
+class Book(BaseModel):
+    title: constr(min_length=1, max_length=100)
+    page_count: conint(gt=0, lt=10000)
+
+class Library(BaseModel):
+    name: constr(min_length=3, max_length=50)
+    books: List[Book]
